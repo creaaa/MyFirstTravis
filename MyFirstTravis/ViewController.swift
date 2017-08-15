40b2e364ed5719e13f7e7f5c1c682cf18d28e071
crea <masataka0504@gmail.com>
2017-08-15 00:23:58 -0700

    tmp

--- MyFirstTravis/ViewController.swift
+++ MyFirstTravis/ViewController.swift
@@ -1,14 +1,28 @@
 
 import UIKit
 
+/**
+ 
+ UIViewControllerを継承したViewControllerクラス
+ 
+ */
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
     }
 
+    
+    // MARK: Method
+    /**
+     
+     ログを出力するメソッド
+     
+     - parameter message: ログ出力したいメッセージ
+     
+     */
+    
     func hoge() {
         print("hoge!")
     }
